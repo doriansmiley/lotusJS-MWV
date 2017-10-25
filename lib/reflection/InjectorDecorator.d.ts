41930ec13d238b966aa7aa29911d4abf71d3fcec
dorian smiley <doriansmiley@yahoo.com>
2017-10-25 14:17:31 -0700

    merge

--- lib/reflection/InjectorDecorator.d.ts
+++ lib/reflection/InjectorDecorator.d.ts
@@ -2,10 +2,12 @@
  * Created by dsmiley on 8/30/17.
  */
 import 'reflect-metadata';
-import { IInjector } from "../context/IInjector";
 export declare type injectionResolver = {
     property: string;
     type: Function;
+    context: string;
+    instance: any;
 };
-export declare function inject(injector: IInjector): any;
+export declare const injections: Array<injectionResolver>;
+export declare function inject(context: string): any;
 export declare function bindable(): any;
